2d675dda259e6962fc80711bb051097182f35896
Delici0u-s <maxi.bravo111@gmail.com>
2025-10-13 00:27:40 +0200

    smol progrez, still implementing

--- include/de_linked_list.h
+++ include/de_linked_list.h
@@ -6,6 +6,9 @@ extern "C" {
 
 /*
 to get function definitions include DE_CONTAINER_LLIST_IMPLEMENTATION before the #include
+simple implementation of Doubly Linked List, may be changed to an indext doubly linked list or sth, all change will
+happen internally though, so exposed function will stay the same, ensuring if only the funcions are used as provided
+behaviour will not change
 */
 
 /* possible options to set before 'first' include */
@@ -13,10 +16,10 @@ to get function definitions include DE_CONTAINER_LLIST_IMPLEMENTATION before the
 #ifdef DE_CONTAINER_LLIST_OPTIONS
 /* no options */
 #define DE_OPTIONS_LLIST_NO_SAFETY_ASSERTS
-#define DE_OPTIONS_LLIST_MALLOC_FUNCTION(size) defaults to malloc from stdlib.h
-#define DE_OPTIONS_LLIST_FREE_FUNCTION(ptr) defaults to free from stdlib.h
-#define DE_OPTIONS_LLIST_MEMCPY_FUNCTION(src, dest, size) defaults to memcpy from string.h
-#define DE_OPTIONS_LLIST_ASSERT_FUNCTION(to_assert) defaults to assert from assert.h
+#define DE_OPTIONS_LLIST_MALLOC_FUNCTION defaults to malloc from stdlib.h
+#define DE_OPTIONS_LLIST_FREE_FUNCTION defaults to free from stdlib.h
+#define DE_OPTIONS_LLIST_MEMCPY_FUNCTION defaults to memcpy from string.h
+#define DE_OPTIONS_LLIST_ASSERT_FUNCTION defaults to assert from assert.h
 #endif
 #endif
 
@@ -34,7 +37,9 @@ to get function definitions include DE_CONTAINER_LLIST_IMPLEMENTATION before the
 /* declarations */
 /* de_llist stands for de_linked_list */
 
-// TODO: Add find, remove, merge, split, splice, improve node data allocation, ggf wirth tombstone llist
+typedef u0 (*de_llist_del_func)(u0 *const _item);
+
+// TODO: Add find, remove, merge, split, splice, improve node data allocation, ggf with tombstone llist
 typedef struct de_llist_node {
   void *data;
   struct de_llist_node *next;
@@ -48,33 +53,41 @@ usize item_size;
 usize size;
 } de_llist;
 
-// typedef bool (*de_llist_val_cmp_func)(const u0 *item, u0 *data);
+// typedef bool (*de_llist_val_cmp_func)(const u0 *const item, u0 *const data);
 
+// basic api, will not be changed, but may be expanded in the future
+DE_CONTAINER_LLIST_API de_llist de_llist_create(const usize _item_size);
+DE_CONTAINER_LLIST_API u0 de_llist_destroy(de_llist* const _ll, de_llist_del_func _free_func);
 
-de_llist de_llist_create(const usize _item_size);
-u0 de_llist_destroy(de_llist* const _ll);
+DE_CONTAINER_LLIST_API u0 *de_llist_remove(de_llist* const _ll, const usize _idx);
+DE_CONTAINER_LLIST_API u0 *de_llist_remove_node(de_llist* const _ll, de_llist_node* _node);
+DE_CONTAINER_LLIST_API u0 *de_llist_pop_back(de_llist* const _ll);
+DE_CONTAINER_LLIST_API u0 *de_llist_pop_front(de_llist* const _ll);
 
-u0 *de_llist_remove(de_llist* const _ll, const usize _idx);
-u0 *de_llist_pop_back(de_llist* const _ll);
-u0 *de_llist_pop_front(de_llist* const _ll);
+DE_CONTAINER_LLIST_API u0* de_llist_get(de_llist* const _ll, const usize _idx);
+DE_CONTAINER_LLIST_API de_llist_node* de_llist_get_node(de_llist* const _ll, const usize _idx);
+DE_CONTAINER_LLIST_API u0* de_llist_get_front(de_llist* const _ll);
+DE_CONTAINER_LLIST_API u0* de_llist_get_back(de_llist* const _ll);
 
-u0* de_llist_get(de_llist* const _ll, const usize _idx);
-u0* de_llist_get_front(de_llist* const _ll);
-u0* de_llist_get_back(de_llist* const _ll);
+/* memcpys data */
+DE_CONTAINER_LLIST_API u0 de_llist_push_back(de_llist* const _ll,const  void*const _data);
+DE_CONTAINER_LLIST_API u0 de_llist_push_front(de_llist* const _ll, const void*const _data);
+DE_CONTAINER_LLIST_API u0 de_llist_insert(de_llist* const _ll, const usize _idx,const void*const _data);
 
-u0 de_llist_push_back(de_llist* const _ll, void* _data);
-u0 de_llist_push_front(de_llist* const _ll, void* _data);
-u0 de_llist_insert(de_llist* const _ll, const usize _idx, void* _data);
+/* copies the data pointer, not the contents. Sets original pointer to NULL */
+DE_CONTAINER_LLIST_API u0 de_llist_push_back_mv(de_llist* const _ll, void** _data);
+DE_CONTAINER_LLIST_API u0 de_llist_push_front_mv(de_llist* const _ll, void** _data);
+DE_CONTAINER_LLIST_API u0 de_llist_insert_mv(de_llist* const _ll, const usize _idx, void** _data);
 
-usize de_llist_info_size(de_llist* const _ll);
-usize de_llist_info_item_size(de_llist* const _ll);
+DE_CONTAINER_LLIST_API usize de_llist_info_size(de_llist* const _ll);
+DE_CONTAINER_LLIST_API usize de_llist_info_item_size(de_llist* const _ll);
 
-de_llist_node* de_llist_begin(de_llist* _ll);
-de_llist_node* de_llist_end(de_llist* _ll); // maybe NULL
+DE_CONTAINER_LLIST_API de_llist_node* de_llist_begin(de_llist* _ll);
+DE_CONTAINER_LLIST_API de_llist_node* de_llist_end(de_llist* _ll); // maybe NULL
 
-de_llist_node* de_llist_next(de_llist_node* _node);
-de_llist_node* de_llist_prev(de_llist_node* _node);
-u0* de_llist_node_data(de_llist_node* _node);
+DE_CONTAINER_LLIST_API de_llist_node* de_llist_next(de_llist_node* _node);
+DE_CONTAINER_LLIST_API de_llist_node* de_llist_prev(de_llist_node* _node);
+DE_CONTAINER_LLIST_API u0* de_llist_node_data(de_llist_node* _node);
 
 /* clang-format on */
 #ifdef __cplusplus
@@ -96,25 +109,170 @@ extern "C" {
 #include <assert.h>
 
 #ifndef DE_OPTIONS_LLIST_MALLOC_FUNCTION
-#define DE_OPTIONS_LLIST_MALLOC_FUNCTION(size) malloc(size)
+#define DE_OPTIONS_LLIST_MALLOC_FUNCTION malloc
 #endif
 #ifndef DE_OPTIONS_LLIST_FREE_FUNCTION
-#define DE_OPTIONS_LLIST_FREE_FUNCTION(ptr) free(ptr)
+#define DE_OPTIONS_LLIST_FREE_FUNCTION free
 #endif
 #ifndef DE_OPTIONS_LLIST_MEMCPY_FUNCTION
-#define DE_OPTIONS_LLIST_MEMCPY_FUNCTION(src, dest, size) memcpy(src, dest, size)
+#define DE_OPTIONS_LLIST_MEMCPY_FUNCTION memcpy
 #endif
 #ifndef DE_OPTIONS_LLIST_ASSERT_FUNCTION
-#define DE_OPTIONS_LLIST_ASSERT_FUNCTION(to_assert) assert(to_assert)
+#define DE_OPTIONS_LLIST_ASSERT_FUNCTION assert
 #endif
 
-#define DE_C_LL_MALLOC(size) DE_OPTIONS_LLIST_MALLOC_FUNCTION(size)
-#define DE_C_LL_FREE(ptr) DE_OPTIONS_LLIST_FREE_FUNCTION(ptr)
-#define DE_C_LL_MEMCPY(src, dest, size) DE_OPTIONS_LLIST_MEMCPY_FUNCTION(src, dest, size)
-#define DE_C_LL_ASSERT(to_assert) DE_OPTIONS_LLIST_ASSERT_FUNCTION(to_assert)
+#define DE_C_LL_MALLOC DE_OPTIONS_LLIST_MALLOC_FUNCTION
+#define DE_C_LL_FREE DE_OPTIONS_LLIST_FREE_FUNCTION
+#define DE_C_LL_MEMCPY DE_OPTIONS_LLIST_MEMCPY_FUNCTION
+#define DE_C_LL_ASSERT DE_OPTIONS_LLIST_ASSERT_FUNCTION
 
 /* implementations */
+DE_CONTAINER_LLIST_INTERNAL de_llist de_llist_create(const usize _item_size) { //
+  return (de_llist){.item_size = _item_size, .size = 0, .head = NULL, .tail = NULL};
+}
 
+DE_CONTAINER_LLIST_INTERNAL u0 de_llist_destroy(de_llist *const _ll, de_llist_del_func _free_func_or_NULL) {
+#ifndef DE_OPTIONS_LLIST_NO_SAFETY_ASSERTS
+  DE_C_LL_ASSERT(_ll != NULL);
+#endif
+
+  if (_free_func_or_NULL == NULL) { _free_func_or_NULL = DE_C_LL_FREE; }
+
+  for (de_llist_node *cur = _ll->head, *next; cur; cur = next) {
+    next = cur->next;
+    _free_func_or_NULL(cur->data);
+    DE_C_LL_FREE(cur);
+  }
+
+  *_ll = (de_llist){0};
+}
+
+DE_CONTAINER_LLIST_INTERNAL u0 *de_llist_remove(de_llist *const _ll, const usize _idx) { //
+#ifndef DE_OPTIONS_LLIST_NO_SAFETY_ASSERTS
+  DE_C_LL_ASSERT(_ll != NULL);
+  DE_C_LL_ASSERT(_idx < _ll->size);
+#endif
+  de_llist_node *_node = de_llist_get_node(_ll, _idx);
+  return de_llist_remove_node(_ll, _node);
+}
+
+DE_CONTAINER_LLIST_INTERNAL u0 *de_llist_remove_node(de_llist *const _ll, de_llist_node *_node) {
+#ifndef DE_OPTIONS_LLIST_NO_SAFETY_ASSERTS
+  DE_C_LL_ASSERT(_ll != NULL);
+  DE_C_LL_ASSERT(_node != NULL);
+#endif
+
+  de_llist_node *prev = _node->prev;
+  de_llist_node *next = _node->next;
+
+  if (prev) prev->next = next;
+  else _ll->head = next;
+
+  if (next) next->prev = prev;
+  else _ll->tail = prev;
+
+  u0 *data = _node->data;
+  DE_C_LL_FREE(_node);
+  --_ll->size;
+
+  return data; // head/tail are already correct
+}
+
+DE_CONTAINER_LLIST_INTERNAL u0 *de_llist_pop_back(de_llist *const _ll) { //
+#ifndef DE_OPTIONS_LLIST_NO_SAFETY_ASSERTS
+  DE_C_LL_ASSERT(_ll != NULL);
+  DE_C_LL_ASSERT(_ll->size != 0);
+#endif
+
+  return de_llist_remove_node(_ll, _ll->tail);
+}
+DE_CONTAINER_LLIST_INTERNAL u0 *de_llist_pop_front(de_llist *const _ll) { //
+#ifndef DE_OPTIONS_LLIST_NO_SAFETY_ASSERTS
+  DE_C_LL_ASSERT(_ll != NULL);
+  DE_C_LL_ASSERT(_ll->size != 0);
+#endif
+
+  return de_llist_remove_node(_ll, _ll->head);
+}
+
+de_llist_node *de_llist_get_node(de_llist *const _ll, const usize _idx) {
+#ifndef DE_OPTIONS_LLIST_NO_SAFETY_ASSERTS
+  DE_C_LL_ASSERT(_ll != NULL);
+  DE_C_LL_ASSERT(_idx < _ll->size);
+#endif
+
+  const usize middle = _ll->size / 2;
+  de_llist_node *cur;
+
+  if (_idx < middle) {
+    cur = _ll->head;
+    // advance _idx times from head to reach index _idx
+    for (usize i = 0; i < _idx; ++i) {
+      cur = cur->next;
+    }
+  } else {
+    cur = _ll->tail;
+    // start from last index (_ll->size - 1) and step backwards until _idx
+    for (usize i = _ll->size - 1; i > _idx; --i) {
+      cur = cur->prev;
+    }
+  }
+
+  return cur;
+}
+
+DE_CONTAINER_LLIST_INTERNAL u0 *de_llist_get(de_llist *const _ll, const usize _idx) { //
+  return de_llist_get_node(_ll, _idx)->data;
+}
+
+DE_CONTAINER_LLIST_INTERNAL u0 *de_llist_get_front(de_llist *const _ll) { //
+#ifndef DE_OPTIONS_LLIST_NO_SAFETY_ASSERTS
+  DE_C_LL_ASSERT(_ll != NULL);
+  DE_C_LL_ASSERT(_ll->size != 0);
+#endif
+  return _ll->head->data;
+}
+DE_CONTAINER_LLIST_INTERNAL u0 *de_llist_get_back(de_llist *const _ll) { //
+#ifndef DE_OPTIONS_LLIST_NO_SAFETY_ASSERTS
+  DE_C_LL_ASSERT(_ll != NULL);
+  DE_C_LL_ASSERT(_ll->size != 0);
+#endif
+  return _ll->tail->data;
+}
+
+/* memcpys data */
+DE_CONTAINER_LLIST_INTERNAL u0 de_llist_push_back(de_llist *const _ll, const void *const _data) { //
+}
+DE_CONTAINER_LLIST_INTERNAL u0 de_llist_push_front(de_llist *const _ll, const void *const _data) { //
+}
+DE_CONTAINER_LLIST_INTERNAL u0 de_llist_insert(de_llist *const _ll, const usize _idx, const void *const _data) { //
+}
+
+/* copies the data pointer, not the contents. Sets original pointer to NULL */
+DE_CONTAINER_LLIST_INTERNAL u0 de_llist_push_back_mv(de_llist *const _ll, void **_data) { //
+}
+DE_CONTAINER_LLIST_INTERNAL u0 de_llist_push_front_mv(de_llist *const _ll, void **_data) { //
+}
+DE_CONTAINER_LLIST_INTERNAL u0 de_llist_insert_mv(de_llist *const _ll, const usize _idx, void **_data) { //
+}
+
+DE_CONTAINER_LLIST_INTERNAL usize de_llist_info_size(de_llist *const _ll) { //
+}
+DE_CONTAINER_LLIST_INTERNAL usize de_llist_info_item_size(de_llist *const _ll) { //
+}
+
+DE_CONTAINER_LLIST_INTERNAL de_llist_node *de_llist_begin(de_llist *_ll) { //
+}
+DE_CONTAINER_LLIST_INTERNAL de_llist_node *de_llist_end(de_llist *_ll) { //
+
+} // maybe NULL
+
+DE_CONTAINER_LLIST_INTERNAL de_llist_node *de_llist_next(de_llist_node *_node) { //
+}
+DE_CONTAINER_LLIST_INTERNAL de_llist_node *de_llist_prev(de_llist_node *_node) { //
+}
+DE_CONTAINER_LLIST_INTERNAL u0 *de_llist_node_data(de_llist_node *_node) { //
+}
 #ifdef __cplusplus
 } // extern "C"
 #endif
--- src/main.c
+++ src/main.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
-int main(void) { puts("hi"); }
+
+int *a(int **b) {
+  int *c = *b;
+  *b = NULL;
+  return c;
+}
+
+int main(void) {
+  int b = 2;
+  int *c = &b;
+  printf("%p : %i\n", (void *)c, *c);
+
+  int *d = a(&b);
+
+  // Always check for NULL before dereferencing
+  if (c == NULL) {
+    printf("%p : (null)\n", (void *)c);
+  } else {
+    printf("%p : %i\n", (void *)c, *c);
+  }
+
+  printf("%p : %i\n", (void *)d, *d);
+
+  puts("hi");
+  return 0;
+}
